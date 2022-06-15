dd240362bd98a8c4be1754c0dd5ce8522683032f
Fridabug <friederikespring@posteo.de>
2022-06-15 11:32:55 +0200

    .

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -5,6 +5,29 @@ function Home() {
   return (
     <div className='container main'>
       <h1>Friederike Spring</h1>
+      <section class="top">
+        {/* <!-- Intro --> */}
+        <div class="intro">
+          <h1>Hey, I'm <span>Frida</span>.</h1>
+          <h1>I'm a Full Stack Developer building digital solutions.</h1>
+          <p>Let's work on a project together.</p>
+        </div>
+      </section>  
+    {/* about me */}
+      <section class="about-me" id="about-me">
+        <div class="about-me-img">
+        </div>
+        <div class="about-me-txt">
+          <h3>A BIT ABOUT ME</h3>
+          <p>
+            I am an experienced illustrator and skillful web developer. I work responsibly and I am team-oriented. Staying
+            motivated and curious facing problems is one of my strong features. Teamwork boosts my productivity. I enjoy
+            exploring possibilities and finding elegant solutions.
+            My human interactions are shaped by empathy. As with my ability to provide constructive criticism, receive it,
+            and take it to heart.
+          </p>
+        </div>
+  </section>
     </div>
   )
 }
