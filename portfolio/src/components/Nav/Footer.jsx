326689524ef91311303ddd70b31b14676cf02c74
Fridabug <friederikespring@posteo.de>
2022-06-23 13:28:32 +0200

    add letter icon

--- portfolio/src/components/Nav/Footer.jsx
+++ portfolio/src/components/Nav/Footer.jsx
@@ -3,12 +3,18 @@ import { Link } from 'react-router-dom';
 import { GiEarthAfricaEurope } from 'react-icons/gi';
 import { BsGithub } from 'react-icons/bs';
 import { AiFillLinkedin } from 'react-icons/ai'
+import { MdOutlineLocalPostOffice } from 'react-icons/md';
 
 
 function Footer() {
   return (
     <footer className="navbar-dark bg-dark" id="contact"> 
       <ul>
+        <li class="nav-item">
+          <a href="mailto:friederikespring@posteo.de" target="_blank">
+            <MdOutlineLocalPostOffice/>
+          </a>
+        </li>
         <li className='nav-item'>
           <a className='nav-link' target='_blank' href='https://github.com/Fridabug'>
             <BsGithub/>
